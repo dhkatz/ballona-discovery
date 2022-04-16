3614c3f6b33b329181c47bdeb4786abc0a9af880
dhkatz <dhkatz.16@gmail.com>
2022-04-15 23:07:48 -0700

    Make query optional for collection hook

--- packages/app/src/hooks/useCollection.ts
+++ packages/app/src/hooks/useCollection.ts
@@ -1,4 +1,4 @@
-import { collection, CollectionReference, Query } from 'firebase/firestore';
+import { collection, CollectionReference, Query, query } from 'firebase/firestore';
 import { User } from 'firebase/auth';
 import { CollectionDataHook, useCollectionData } from 'react-firebase-hooks/firestore';
 
@@ -7,14 +7,14 @@ import { useFirebase } from './useFirebase';
 
 export const useCollection = <T>(
 	collectionName: string,
-	buildQuery: (ref: CollectionReference<T>, auth: User) => Query<T>
+	buildQuery?: (ref: CollectionReference<T>, auth: User) => Query<T>
 ): CollectionDataHook<T> => {
 	const { firestore } = useFirebase();
 	const [auth] = useAuth();
 	const ref = collection(firestore, collectionName) as CollectionReference<T>;
-	const query = auth ? buildQuery(ref, auth) : null;
+	const builtQuery = auth ? (buildQuery ? buildQuery(ref, auth) : query(ref)) : null;
 
-	const [data, loading, error, snapshot] = useCollectionData<T>(query);
+	const [data, loading, error, snapshot] = useCollectionData<T>(builtQuery);
 
 	return [data, loading, error, snapshot];
 };
