dd37bccd9682c1d8c54b68568ea85da357df4e2c
Harrison Roskopp <hrroskopp@gmail.com>
2022-04-17 23:26:16 -0700

    Updated user management

--- packages/app/src/views/Dashboard/UserManagement.tsx
+++ packages/app/src/views/Dashboard/UserManagement.tsx
@@ -4,32 +4,9 @@ import { User } from '../../types';
 import { doc, updateDoc } from 'firebase/firestore';
 import { firestore } from '../../firebase';
 
-// const users = [
-// 	{
-// 		uid: 'xxxxxxxxx',
-// 		firstName: 'richard',
-// 		lastName: 'incognito',
-// 		email: 'richard@example.com',
-// 		role: 'admin',
-// 	},
-// 	{
-// 		uid: 'yyyyyyyyy',
-// 		firstName: 'john',
-// 		lastName: 'incognito',
-// 		email: 'john@example.com',
-// 		role: 'editor',
-// 	},
-// 	{
-// 		uid: 'zzzzzzzzz',
-// 		firstName: 'jack',
-// 		lastName: 'incognito',
-// 		email: 'jack@example.com',
-// 		role: 'editor',
-// 	},
-// ];
-
 export const UserManagement = () => {
 	const [users, loading, error] = useCollection<User>('users');
+	console.log(users);
 
 	if (error) {
 		console.log(error);
@@ -44,47 +21,52 @@ export const UserManagement = () => {
 
 	return (
 		<div>
-			{loading && <p>loading</p>}
-			<Table striped bordered hover variant="dark">
-				<thead>
-					<tr>
-						<th>User ID</th>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Email</th>
-						<th>Role</th>
-						<th>Permissions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users &&
-						users.map((user) => {
-							<tr>
-								<td>{user.uid}</td>
-								<td>{user.firstName}</td>
-								<td>{user.lastName}</td>
-								<td>{user.email}</td>
-								<td>{user.role}</td>
-								<td>
-									{/* <Button variant="primary" onClick={() => editPermissions(user)}>
+			<h1>User Management</h1>
+			{loading ? (
+				<p>loading...</p>
+			) : (
+				<Table striped bordered hover variant="dark">
+					<thead>
+						<tr>
+							<th>User ID</th>
+							<th>First Name</th>
+							<th>Last Name</th>
+							<th>Email</th>
+							<th>Role</th>
+							<th>Permissions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users?.map((user) => {
+							return (
+								<tr>
+									<td>{user.uid}</td>
+									<td>{user.firstName}</td>
+									<td>{user.lastName}</td>
+									<td>{user.email}</td>
+									<td>{user.role}</td>
+									<td>
+										{/* <Button variant="primary" onClick={() => editPermissions(user)}>
 										Edit
 									</Button> */}
-									<DropdownButton
-										id="dropdown-basic-button"
-										title="Edit"
-										onSelect={(e) => editPermissions(e, user.uid)}
-									>
-										<Dropdown.Item eventKey="Admin">Admin</Dropdown.Item>
-										<Dropdown.Item eventKey="Contributor">
-											Contributor
-										</Dropdown.Item>
-										<Dropdown.Item eventKey="Reader">Reader</Dropdown.Item>
-									</DropdownButton>
-								</td>
-							</tr>;
+										<DropdownButton
+											id="dropdown-basic-button"
+											title="Edit"
+											onSelect={(e) => editPermissions(e, user.uid)}
+										>
+											<Dropdown.Item eventKey="Admin">Admin</Dropdown.Item>
+											<Dropdown.Item eventKey="Contributor">
+												Contributor
+											</Dropdown.Item>
+											<Dropdown.Item eventKey="Reader">Reader</Dropdown.Item>
+										</DropdownButton>
+									</td>
+								</tr>
+							);
 						})}
-				</tbody>
-			</Table>
+					</tbody>
+				</Table>
+			)}
 		</div>
 	);
 };
