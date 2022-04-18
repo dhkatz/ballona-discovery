3c2023010eabe22ee3999b79de4ce1960811447a
dhkatz <dhkatz.16@gmail.com>
2022-04-17 22:56:57 -0700

    Update dashboard layout and begin working on panels

---
+++ packages/app/src/views/Panels/PanelEditor.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+
+/**
+ * Displays a list of the created tour panels.
+ */
+export const PanelEditor: FunctionComponent = () => {
+	return <div>TODO</div>;
+};
---
+++ packages/app/src/views/Panels/PanelList.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+
+/**
+ * Displays a list of the created tour panels.
+ */
+export const PanelList: FunctionComponent = () => {
+	return <div>TODO</div>;
+};
