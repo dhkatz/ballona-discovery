2aed4a7e0ef3a0e28db4c1bfdcce70d8bc57633e
dhkatz <dhkatz.16@gmail.com>
2022-04-18 23:30:51 -0700

    Update dashboard layout and begin working on panels

--- packages/api/src/firestore.ts
+++ packages/api/src/firestore.ts
@@ -1,18 +1,40 @@
 import * as functions from 'firebase-functions';
 import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
 
 const app = initializeApp();
+
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-export const authOnCreate = functions.auth.user().onCreate((user) => {
-	const { email, uid } = user;
+export const authOnCreate = functions.auth.user().onCreate(async ({ email, uid }) => {
+	const role = 'user';
 
-	return db.collection('users').doc(uid).set({ email });
+	console.log(`Creating user ${email} with role ${role}`);
+
+	await auth.setCustomUserClaims(uid, {
+		role,
+	});
+
+	return db.collection('users').doc(uid).set({ email, role });
 });
 
-export const authOnDelete = functions.auth.user().onDelete((user) => {
-	const { uid } = user;
+export const authOnDelete = functions.auth.user().onDelete(({ uid }) => {
+	console.log(`Deleting user ${uid}`);
 
 	return db.collection('users').doc(uid).delete();
 });
+
+export const updateClaims = functions.firestore
+	.document('users/{userId}')
+	.onUpdate((change, { params }) => {
+		const value = change.after.data();
+		const claims = {
+			role: value.role,
+		};
+
+		console.log(`Updating claims for ${params.userId} to ${JSON.stringify(claims)}`);
+
+		return auth.setCustomUserClaims(params.userId, claims);
+	});
