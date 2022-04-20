97ea49243ad94ea769d17341eb3af4157f37cae6
dhkatz <dhkatz.16@gmail.com>
2022-04-20 12:16:28 -0700

    Clean up role management and add loading indicator

--- packages/app/src/views/Roles/RoleManagement.tsx
+++ packages/app/src/views/Roles/RoleManagement.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import { TailSpin } from 'react-loader-spinner';
 
 import { useCollection } from '../../hooks';
 import { Role } from '../../types';
@@ -10,7 +11,18 @@ import { RoleForm } from './RoleForm';
 export const RoleManagement = () => {
 	const [displayForm, setDisplayForm] = useState(false);
 	const [roleID, setRoleID] = useState<string | null>(null);
-	const [roles, { error, loading, update, add }] = useCollection<Role>('roles');
+	const [roles, { loading, update, add }] = useCollection<Role>('roles');
+
+	if (loading) {
+		return (
+			<>
+				<h1 className="py-4">Role Management</h1>
+				<div className="d-flex justify-content-center">
+					<TailSpin color={'#192A6BFF'} />
+				</div>
+			</>
+		);
+	}
 
 	async function updateRole(id: string, name: string, permissions: string[]) {
 		await update(id, { name, permissions }).then(() => {
@@ -25,45 +37,43 @@ export const RoleManagement = () => {
 		});
 	}
 
+	if (displayForm) {
+		return <RoleForm addRole={addRole} updateRole={updateRole} roleID={roleID} />;
+	}
+
 	return (
-		<div>
-			{displayForm ? (
-				<RoleForm addRole={addRole} updateRole={updateRole} roleID={roleID} />
-			) : (
-				<>
-					<h1>Role Management</h1>
-					<Button onClick={() => setDisplayForm(true)}>Create</Button>
-					<Table striped bordered hover variant="dark">
-						<thead>
+		<>
+			<h1 className="py-4">Role Management</h1>
+			<Button onClick={() => setDisplayForm(true)}>Create</Button>
+			<Table striped bordered hover variant="dark">
+				<thead>
+					<tr>
+						<th>Role Name</th>
+						<th>Permissions</th>
+						<th>Modify</th>
+					</tr>
+				</thead>
+				<tbody>
+					{roles?.map((role) => {
+						return (
 							<tr>
-								<th>Role Name</th>
-								<th>Permissions</th>
-								<th>Modify</th>
+								<td>{role.name}</td>
+								<td>{role.permissions.join(',')}</td>
+								<td>
+									<Button
+										onClick={() => {
+											setRoleID(role.id);
+											setDisplayForm(true);
+										}}
+									>
+										Edit
+									</Button>
+								</td>
 							</tr>
-						</thead>
-						<tbody>
-							{roles?.map((role) => {
-								return (
-									<tr>
-										<td>{role.name}</td>
-										<td>{role.permissions.join(',')}</td>
-										<td>
-											<Button
-												onClick={() => {
-													setRoleID(role.id);
-													setDisplayForm(true);
-												}}
-											>
-												Edit
-											</Button>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</Table>
-				</>
-			)}
-		</div>
+						);
+					})}
+				</tbody>
+			</Table>
+		</>
 	);
 };
